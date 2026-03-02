3094d525deb08ba945c807b5cf9ca589fa1529fc
NinjaSuhani <suhanifutane@gmail.com>
2026-03-02 16:04:20 +0530

    added code

---
+++ main.js
@@ -0,0 +1,11 @@
+let choices = ['rock']
+function playGame(userChoice) {
+    let ran
+    console.log("user choice is," +userChoice );
+
+    const computerChoice = 
+//who is wining
+if(userChoice === computerChoice){
+    //  
+    }
+}
