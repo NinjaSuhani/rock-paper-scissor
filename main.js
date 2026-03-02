c7be445dce2866851ea57f1e8785b09cac19ac0f
NinjaSuhani <suhanifutane@gmail.com>
2026-03-02 17:33:14 +0530

    project done

--- main.js
+++ main.js
@@ -1,11 +1,31 @@
-let choices = ['rock']
+let choices = ["rock", "paper", "scissors"];
+let userScore = 0;
+let computerScore = 0;
 function playGame(userChoice) {
-    let ran
-    console.log("user choice is," +userChoice );
+  let randomIndex = Math.floor(Math.random() * 3);
+  let computerChoice = choices[randomIndex];
 
-    const computerChoice = 
-//who is wining
-if(userChoice === computerChoice){
-    //  
-    }
+  document.getElementById("userChoice").innerText =
+    "Your choice: " + userChoice;
+
+  document.getElementById("computerChoice").innerText =
+    "Computer choice: " + computerChoice;
+
+  if (userChoice === computerChoice) {
+    document.getElementById("finalResult").innerText = "Result: Draw 😐";
+  } else if (
+    (userChoice === "rock" && computerChoice === "scissor") ||
+    (userChoice === "paper" && computerChoice === "rock") ||
+    (userChoice === "scissors" && computerChoice === "paper")
+  ) {
+    userScore++;
+    document.getElementById("finalResult").innerText = "Result: You Win 🎉";
+  } else {
+    computerScore++;
+    document.getElementById("finalResult").innerText =
+      "Result: Computer Wins 🤖";
+  }
+
+  document.getElementById("userScore").innerText = userScore;
+  document.getElementById("computerScore").innerText = computerScore;
 }
